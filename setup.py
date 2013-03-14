36ca43ff3d78601e786f5a2005c01b318304aa4e
chris <chris@mail.aircooledrescue.com>
2013-03-13 22:44:39 -0700

    I'm a big idiot

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-long_description = open('README.md').read()
+long_description = open('README.rst').read()
 
 setup(name='corpcrawl',
-      version='0.0.1',
+      version='0.0.35',
       author="rozap",
       author_email="chrisd1891@gmail.com",
       license="MIT",
-      url="none",
+      url="https://github.com/chrisd1891/corpcrawl/",
       description="Corpcrawl EDGAR Scraper",
       long_description=long_description,
       platforms=['any'],
-      packages=["corpcrawl"]
+      packages=[
+      	'corpcrawl',
+      	'corpcrawl.util',
+      	'corpcrawl.models'
+      ]
 )
